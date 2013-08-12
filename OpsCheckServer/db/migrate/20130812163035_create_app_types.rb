37a25fad3a6f877634d70fb2bc99a77cb52448be
Giuseppe Macrì <giuseppem@slalom.com>
2013-08-12 10:59:12 -0700

    Replaced app_type name with type

--- OpsCheckServer/db/migrate/20130812163035_create_app_types.rb
+++ OpsCheckServer/db/migrate/20130812163035_create_app_types.rb
@@ -1,7 +1,7 @@
 class CreateAppTypes < ActiveRecord::Migration
   def change
     create_table :app_types do |t|
-      t.string :name
+      t.string :type
       t.string :identifier
       t.timestamps
     end
