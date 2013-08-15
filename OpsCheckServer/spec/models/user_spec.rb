0135e4ba31a5104606e670d086be9a20df4de9f8
Giuseppe Macrì <giuseppem@slalom.com>
2013-08-15 16:33:54 -0700

    OPSCHK-20 #comment Added User unit tests using factory girl

--- OpsCheckServer/spec/models/user_spec.rb
+++ OpsCheckServer/spec/models/user_spec.rb
@@ -29,74 +29,74 @@ describe User do
     end
   end
 
-  #it 'should reject invalid email addresses' do
-  #  addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
-  #  addresses.each do |address|
-  #    invalid_email_user = User.new(@attr.merge(:email => address))
-  #    invalid_email_user.not_to be_valid
-  #  end
-  #end
-  #
-  #it 'should reject duplicate email addresses' do
-  #  User.create!(@attr)
-  #  user_with_duplicate_email = User.new(@attr)
-  #  user_with_duplicate_email.not_to be_valid
-  #end
-  #
-  #it 'should reject email addresses identical up to case' do
-  #  upcased_email = @attr[:email].upcase
-  #  User.create!(@attr.merge(:email => upcased_email))
-  #  user_with_duplicate_email = User.new(@attr)
-  #  user_with_duplicate_email.not_to be_valid
-  #end
-  #
-  #describe 'passwords' do
-  #
-  #  before(:each) do
-  #    @user = User.new(@attr)
-  #  end
-  #
-  #  it 'should have a password attribute' do
-  #    @user.to respond_to(:password)
-  #  end
-  #
-  #  it 'should have a password confirmation attribute' do
-  #    @user.to respond_to(:password_confirmation)
-  #  end
-  #end
-  #
-  #describe 'password validations' do
-  #
-  #  it 'should require a password' do
-  #    User.new(@attr.merge(:password => '', :password_confirmation => '')).not_to be_valid
-  #  end
-  #
-  #  it 'should require a matching password confirmation' do
-  #    User.new(@attr.merge(:password_confirmation => 'invalid')).not_to be_valid
-  #  end
-  #
-  #  it 'should reject short passwords' do
-  #    short = 'a' * 5
-  #    hash = @attr.merge(:password => short, :password_confirmation => short)
-  #    User.new(hash).not_to be_valid
-  #  end
-  #
-  #end
-  #
-  #describe 'password encryption' do
-  #
-  #  before(:each) do
-  #    @user = User.create!(@attr)
-  #  end
-  #
-  #  it 'should have an encrypted password attribute' do
-  #    @user.should respond_to(:encrypted_password)
-  #  end
-  #
-  #  it 'should set the encrypted password attribute' do
-  #    @user.encrypted_password.not_to be_blank
-  #  end
-  #
-  #end
+  it 'should reject invalid email addresses' do
+    addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
+    addresses.each do |address|
+      invalid_email_user = User.new(@attr.merge(:email => address))
+      expect(invalid_email_user).not_to be_valid
+    end
+  end
+
+  it 'should reject duplicate email addresses' do
+    User.create!(@attr)
+    user_with_duplicate_email = User.new(@attr)
+    expect(user_with_duplicate_email).not_to be_valid
+  end
+
+  it 'should reject email addresses identical up to case' do
+    upcased_email = @attr[:email].upcase
+    User.create!(@attr.merge(:email => upcased_email))
+    user_with_duplicate_email = User.new(@attr)
+    expect(user_with_duplicate_email).not_to be_valid
+  end
+
+  describe 'passwords' do
+
+    before(:each) do
+      @user = FactoryGirl.create(:user)
+    end
+
+    it 'should have a password attribute' do
+      expect(@user).to respond_to(:password)
+    end
+
+    it 'should have a password confirmation attribute' do
+      expect(@user).to respond_to(:password_confirmation)
+    end
+  end
+
+  describe 'password validations' do
+
+    it 'should require a password' do
+      expect(User.new(@attr.merge(:password => '', :password_confirmation => ''))).not_to be_valid
+    end
+
+    it 'should require a matching password confirmation' do
+      expect(User.new(@attr.merge(:password_confirmation => 'invalid'))).not_to be_valid
+    end
+
+    it 'should reject short passwords' do
+      short = 'a' * 5
+      hash = @attr.merge(:password => short, :password_confirmation => short)
+      expect(User.new(hash)).not_to be_valid
+    end
+
+  end
+
+  describe 'password encryption' do
+
+    before(:each) do
+      @user = FactoryGirl.create(:user)
+    end
+
+    it 'should have an encrypted password attribute' do
+      expect(@user).to respond_to(:encrypted_password)
+    end
+
+    it 'should set the encrypted password attribute' do
+      expect(@user.encrypted_password).not_to be_blank
+    end
+
+  end
 
 end
