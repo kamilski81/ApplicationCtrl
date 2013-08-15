550cf277c59982310c2cca3fa6148d36e7734de6
Giuseppe Macrì <giuseppem@slalom.com>
2013-08-15 12:07:10 -0700

    Added the OpsCheck SDK

--- iOS/OpsCheckSample/OpsCheckSample/OCSAppDelegate.m
+++ iOS/OpsCheckSample/OpsCheckSample/OCSAppDelegate.m
@@ -15,7 +15,24 @@
 {
     
     OpsCheck *opsCheck = [OpsCheck opsCheckWithAppKey:@"d44992361e2014c8404f920f36928dada60a27c4"];
-    [opsCheck checkVersion];
+    
+//    [opsCheck checkSyncVersionWithCompletionHandler:^(BOOL connect, NSInteger status, NSString *message, NSError *error) {
+//        NSLog(@"Sync");
+//        NSLog(@"OpsCheck info: %@", [opsCheck info]);
+//        NSLog(@"Status: %d - Message: %@", status, message);
+//        NSLog(@"Connect: %d", connect);
+//    }];
+//    
+//    [opsCheck checkAsyncVersionWithCompletionHandler:^(BOOL connect, NSInteger status, NSString *message, NSError *error) {
+//        NSLog(@"Sync");
+//        NSLog(@"Error: %@", error);
+//        NSLog(@"OpsCheck info: %@", [opsCheck info]);
+//        NSLog(@"Status: %d - Message: %@", status, message);
+//        NSLog(@"Connect: %d", connect);
+//    }];
+    
+//    [opsCheck checkSyncVersionWithCompletionHandler:nil];
+    [opsCheck checkAsyncVersionWithCompletionHandler:nil];
     
     return YES;
 }
