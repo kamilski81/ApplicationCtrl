4f0eaf301ad66d3506046d02220e13b91956f0b2
Aaron Cooley <aaron@kineticanalysis.com>
2013-09-03 20:27:44 -0700

    Adding a bunch of missing files...
    No JavaDoc
    No UnitTest

---
+++ Android/SampleApp/src/com/example/ApplicationCtrlSampleApp/MyActivity.java
@@ -0,0 +1,22 @@
+package com.example.ApplicationCtrlSampleApp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.slalomdigital.applicationctrl.ApplicationCtrl;
+
+public class MyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Start the ops check...
+        ApplicationCtrl.check(this);
+        // Go on about our business; if the check fails it will launch an activity
+
+        setContentView(R.layout.main);
+    }
+}
